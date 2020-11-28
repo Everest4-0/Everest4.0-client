f09be9e5e327bb8ca8d88ceba6f3511736deac10
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-11-28 19:51:39 +0100

    sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,23 +1,30 @@
+import { AuthService } from './../services/auth.service';
+import { StorageServices } from './../services/storage.service';
+import { StorageService } from 'ngx-webstorage-service';
 import { Component, OnInit } from '@angular/core';
 
 declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+  for: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Inicio',  icon: 'pe-7s-graph', class: '' },
-    { path: '/diagnostic', title: 'Diagnóstico',  icon:'pe-7s-way', class: '' },
-    { path: '/typography', title: 'Objectivo e estratégia',  icon:'pe-7s-target', class: '' },
-    { path: '/icons', title: 'Competência',  icon:'pe-7s-gym', class: '' },
-    { path: '/icon', title: 'Relatórios e consultas',  icon:'pe-7s-note2', class: '' },
-    { path: '/user', title: 'Perfil do utilizazdor',  icon:'pe-7s-note2', class: 'hidden' },
-    { path: '/maps', title: 'Apoio',  icon:'pe-7s-help1', class: '' },
-   /* { path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
-   /* { path: '/user', title: 'User Profile',  icon:'pe-7s-note2', class: '' },*/
-    { path: '/upgrade', title: 'Mudar para Premium',  icon:'pe-7s-rocket', class: 'active-pro' },
+  { path: '/dashboard', title: 'Inicio', icon: 'pe-7s-graph', class: '', for: '' },
+  { path: '/diagnostic', title: 'Diagnóstico', icon: 'pe-7s-way', class: '', for: '' },
+  { path: '/goals', title: 'Objectivos e planos', icon: 'pe-7s-target', class: '', for: '' },
+  { path: '/icons', title: 'Competência', icon: 'pe-7s-gym', class: 'disabled', for: '' },
+  { path: '/icon', title: 'Relatórios e consultas', icon: 'pe-7s-note2', class: 'disabled', for: '' },
+  { path: '/user', title: 'Cursos online ', icon: 'pe-7s-note2', class: 'disabled', for: '' },
+  { path: '/user', title: 'Coaching online', icon: 'pe-7s-note2', class: 'disabled', for: '' },
+  { path: '/user', title: 'Vagas e gestão de carreiras ', icon: 'pe-7s-note2', class: 'disabled', for: '' },
+  { path: '/user', title: 'Monitorização e relatórios ', icon: 'pe-7s-note2', class: 'disabled', for: '' },
+  { path: '/maps', title: 'Apoio', icon: 'pe-7s-help1', class: '', for: '' },
+  /* { path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
+  /* { path: '/user', title: 'User Profile',  icon:'pe-7s-note2', class: '' },*/
+  { path: '/upgrade', title: 'Mudar para Premium', icon: 'pe-7s-rocket', class: 'active-pro ', for: 'FREE' },
 ];
 
 @Component({
@@ -27,15 +34,20 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
   isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
   };
+
+  signOut() {
+    this.auth.signOut();
+    window.open('/', '_self')
+  }
 }
