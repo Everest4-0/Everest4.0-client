eea639961068d16179bee9e0a5e546b6e0138d81
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-11-28 19:48:47 +0100

    user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,13 +4,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  public isTab = 1;
 
-  constructor(public auth:AuthService) { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
 
+
+  switchtabTo(n: number) {
+    this.isTab=n;
+  }
+  closeModal(ok){
+    
+  }
 }
