af1283ca5f7e90e01b164fdec82bd15f45da17d3
PedroSousa2020 <sousapedro747@gmail.com>
2020-12-17 17:35:18 +0100

    Organizando o form:  todos os campos criados, faltando criar o ngForm

--- src/app/backoffice/user/update-user/update-user.component.ts
+++ src/app/backoffice/user/update-user/update-user.component.ts
@@ -12,6 +12,7 @@ export class UpdateUserComponent implements OnInit {
 
   user: User;
 
+  
   constructor(private userService: UserService,
               private route: ActivatedRoute) { 
               }
