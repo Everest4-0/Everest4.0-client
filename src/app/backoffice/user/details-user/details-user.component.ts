cb8c217f8e821362560c0f5caf6a9d908bc01b2a
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-12-18 10:53:16 +0100

    details

--- src/app/backoffice/user/details-user/details-user.component.ts
+++ src/app/backoffice/user/details-user/details-user.component.ts
@@ -6,26 +6,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-details-user',
   templateUrl: './details-user.component.html',
-  styleUrls: ['./details-user.component.css']
+  styleUrls: ['./details-user.component.scss']
 })
 export class DetailsUserComponent implements OnInit {
 
- 
-  public user:User;
+
+  public user: User;
 
   public isTab = 1;
-  //public user: User = this.auth.user;
-  constructor(public userService:UserService,
-              public route:ActivatedRoute) { }
+  constructor(
+    public userService: UserService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id=this.route.snapshot.params['id'];
-    this.userService.one(id).subscribe(user=>this.user=user);
-  }
-
-
-  switchtabTo(n: number) {
-    this.isTab = n;
+    const id = this.route.snapshot.params['id'];
+    this.userService.one(id).subscribe(user => this.user = user);
   }
 
 }
