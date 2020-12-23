7a5edade4241af0c7266cf0cc49ad8209589bd6d
PedroSousa2020 <sousapedro747@gmail.com>
2020-12-23 15:10:48 +0100

    list of quizes, quiz-form done

--- src/app/frontoffice/quiz/quiz.component.ts
+++ src/app/frontoffice/quiz/quiz.component.ts
@@ -1,3 +1,4 @@
+import { QuizService } from './../../services/quiz.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  public desafios:Number=1;
+
+ constructor(private quizService:QuizService) { }
 
   ngOnInit(): void {
+    this.quizService.all({}).subscribe(quizes=>this.desafios=quizes.length-1)
   }
 
 }
