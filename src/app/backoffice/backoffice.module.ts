8e993bba20683b09137b15d1e1120d5e51046430
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-12-28 08:38:00 +0100

    course fixer, and rich text editor

--- src/app/backoffice/backoffice.module.ts
+++ src/app/backoffice/backoffice.module.ts
@@ -39,6 +39,28 @@ import { CreateCourseComponent } from './courses/create-course/create-course.com
 import { DetailsCourseComponent } from './courses/details-course/details-course.component';
 import { UpdateCourseComponent } from './courses/update-course/update-course.component';
 
+
+import { SwitchModule } from '@syncfusion/ej2-angular-buttons';
+
+import { TabModule } from '@syncfusion/ej2-angular-navigations';
+
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+
+import { TextBoxModule } from '@syncfusion/ej2-angular-inputs';
+
+import { NumericTextBoxModule } from '@syncfusion/ej2-angular-inputs';
+
+import { RadioButtonModule } from '@syncfusion/ej2-angular-buttons';
+
+import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
+
+import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
+
+
 @NgModule({
   declarations: [
     CreateUserComponent,
@@ -79,7 +101,24 @@ import { UpdateCourseComponent } from './courses/update-course/update-course.com
     MomentModule,
     NgSelectModule,
     DragDropModule,
-    ModalModule
+    ModalModule,
+
+
+    /*********************** */
+
+   // BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TabModule,
+    RichTextEditorAllModule,
+    CheckBoxModule,
+    DialogModule,
+    NumericTextBoxModule,
+    ButtonModule,
+    SwitchModule,
+    RadioButtonModule,
+    TextBoxModule,
+    DropDownListModule
   ]
 })
 export class BackofficeModule { }
--- src/app/backoffice/courses/details-course/details-course.component.ts
+++ src/app/backoffice/courses/details-course/details-course.component.ts
@@ -6,12 +6,19 @@ import { ModalService } from './../../../components/modal/modal.service';
 import { Course } from 'app/models/course/course';
 import { ActivatedRoute } from '@angular/router';
 import { CourseService } from './../../../services/courses/course.service';
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { addClass, removeClass, Browser } from '@syncfusion/ej2-base';
+import { ToolbarService, ToolbarType, LinkService, ImageService } from '@syncfusion/ej2-angular-richtexteditor';
+import { DropDownListComponent, FieldSettingsModel } from '@syncfusion/ej2-angular-dropdowns';
+import { CheckBoxComponent } from '@syncfusion/ej2-angular-buttons';
+import { RichTextEditorComponent, HtmlEditorService, QuickToolbarService } from '@syncfusion/ej2-angular-richtexteditor';
+import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
 @Component({
   selector: 'app-details-course',
   templateUrl: './details-course.component.html',
-  styleUrls: ['./details-course.component.scss']
+  styleUrls: ['./details-course.component.scss','./materialize.lite.scss'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService, QuickToolbarService]
 })
 export class DetailsCourseComponent implements OnInit {
 
@@ -19,11 +26,79 @@ export class DetailsCourseComponent implements OnInit {
   public course: Course = new Course();
   public serverAddress = this.courseService.serverAddress;
   activityForm = new ActivityForm()
+
+  model;
+  @ViewChild('type') ddlObj: DropDownListComponent;
+  @ViewChild('float') rteFloatObj: CheckBoxComponent;
+  @ViewChild('typeRTE') rteObj: RichTextEditorComponent;
+  public tools: ToolbarModule = {
+      items: ['Bold', 'Italic', 'Underline', 'StrikeThrough',
+          'FontName', 'FontSize', 'FontColor', 'BackgroundColor',
+          'LowerCase', 'UpperCase', '|',
+          'Formats', 'Alignments', 'OrderedList', 'UnorderedList',
+          'Outdent', 'Indent', '|',
+          'CreateLink', 'Image', '|', 'ClearFormat', 'Print',
+          'SourceCode', 'FullScreen', '|', 'Undo', 'Redo']
+  };
+  // Mapping DropDownList dataSource property
+  public dataSource: { [key: string]: Object }[] = [{ text: 'Expand', value: 1 }, { text: 'MultiRow', value: 2 }];
+  public fltType: string = 'Auto';
+  public placeholder: string = 'Types';
+  // Mapping DropDownList fields property
+  public typeFields: FieldSettingsModel = { text: 'text', value: 'value' };
+  public typeValue: number = 1;
+  // Change event funtion for DropDownList component
+  public typeChange(): void {
+      switch (this.ddlObj.value as number) {
+          case 1:
+              this.rteObj.toolbarSettings.type = ToolbarType.Expand;
+              this.rteObj.toolbarSettings.enableFloating = this.rteFloatObj.checked;
+              break;
+          case 2:
+              this.rteObj.toolbarSettings.type = ToolbarType.MultiRow;
+              this.rteObj.toolbarSettings.enableFloating = this.rteFloatObj.checked;
+              break;
+      }
+  }
+  public onChangeFloat(): void {
+      this.rteObj.toolbarSettings.enableFloating = this.rteFloatObj.checked;
+  }
+  public handleFullScreen(e: any): void {
+      const sbCntEle: HTMLElement = document.querySelector('.sb-content.e-view');
+      const sbHdrEle: HTMLElement = document.querySelector('.sb-header.e-view');
+      const leftBar: HTMLElement = document.querySelector('#left-sidebar');
+      if (e.targetItem === 'Maximize') {
+          if (Browser.isDevice && Browser.isIos) {
+              addClass([sbCntEle, sbHdrEle], ['hide-header']);
+          }
+          addClass([leftBar], ['e-close']);
+          removeClass([leftBar], ['e-open']);
+      } else if (e.targetItem === 'Minimize') {
+          if (Browser.isDevice && Browser.isIos) {
+              removeClass([sbCntEle, sbHdrEle], ['hide-header']);
+          }
+          removeClass([leftBar], ['e-close']);
+          if (!Browser.isDevice) {
+              addClass([leftBar], ['e-open']);
+          }
+      }
+  }
+  public actionCompleteHandler(): void {
+      setTimeout(() => { 
+        this.rteObj.toolbarModule.refreshToolbarOverflow();
+      
+
+        this.rteObj.toolbarSettings.type = ToolbarType.MultiRow;
+        this.rteObj.toolbarSettings.enableFloating = true;
+        
+      }, 400);
+  }
+
   constructor(
     private activityService: ActivityService,
     private courseService: CourseService,
     private route: ActivatedRoute,
-    private modalService: ModalService
+    public modalService: ModalService
   ) { }
 
   ngOnInit(): void {
@@ -62,6 +137,9 @@ export class DetailsCourseComponent implements OnInit {
   }
 
   addActivity(module: Module) {
+
+    this.rteObj.toolbarSettings.type = ToolbarType.MultiRow;
+    this.rteObj.toolbarSettings.enableFloating = true;
     this.activity.module = module;
     this.modalService.open('form-activity-modal');
   }
