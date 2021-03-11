a574b15aa06377dda4695f3d96de9dcc2de7afeb
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2021-03-11 23:33:29 +0100

    coaching module

--- src/app/frontoffice/coaching/coaching.module.ts
+++ src/app/frontoffice/coaching/coaching.module.ts
@@ -5,11 +5,18 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DiagnosticViewComponent } from './diagnostic-view/diagnostic-view.component';
+import { ClientBoardComponent } from './client-board/client-board.component';
+import { CoachingDashboardComponent } from './coaching-dashboard/coaching-dashboard.component';
+import { CoachingEvolutionComponent } from './coaching-evolution/coaching-evolution.component';
+import { ChatComponent } from './chat/chat.component';
 
 
 
 @NgModule({
-  declarations: [CoachingComponent, DiagnosticViewComponent],
+  declarations: [
+    CoachingComponent, 
+    DiagnosticViewComponent, 
+    ClientBoardComponent, CoachingDashboardComponent, CoachingEvolutionComponent, ChatComponent],
   imports: [
     CommonModule, 
     RouterModule.forChild(CoachingRoutes)
--- src/app/frontoffice/coaching/coaching.routing.ts
+++ src/app/frontoffice/coaching/coaching.routing.ts
@@ -1,9 +1,14 @@
+import { CoachingDashboardComponent } from './coaching-dashboard/coaching-dashboard.component';
+import { ClientBoardComponent } from './client-board/client-board.component';
 import { CoachingComponent } from './coaching/coaching.component';
 
 import { Routes } from '@angular/router';
 
 export const CoachingRoutes: Routes = [
     {
-        path: 'coaching', component: CoachingComponent
+        path: '', component: CoachingDashboardComponent
+    },
+    {
+        path: 'board', component: ClientBoardComponent
     }
 ]; 
