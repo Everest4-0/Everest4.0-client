bf992d76ce50ffa521efd65d61698457f60918bf
PedroSousa2020 <sousapedro747@gmail.com>
2021-03-15 08:21:26 +0100

    Fix subscription, in fault save and add toast efect

--- src/app/models/coaching/coaching_subscription.ts
+++ src/app/models/coaching/coaching_subscription.ts
@@ -1,20 +1,19 @@
+import { CoachingDuration } from './coaching_duration';
+import { CoachingGoal } from './coaching_goal';
 import { User } from './../main/user';
 import { Chat } from './../main/chat'
 
 export class CoachingSubscription {
     id: string;
-    code: string;
 
-    title: string;
     descriptions: string;
-
 
     isActive: boolean;
     createdAt: Date;
     updatedAt: Date;
 
-    goal: any;
-    duration: any;
+    goal: CoachingGoal = new CoachingGoal();
+    duration: CoachingDuration = new CoachingDuration();
     user: User = new User();
     coach: User = new User();
     chat: Chat = new Chat()
